636b0f83d13f3a3f82dcad80424dba41e0931f97
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-29 18:25:37 +0530

    Update Readme

--- README.md
+++ README.md
@@ -71,3 +71,18 @@ Nest is an MIT-licensed open source project. It can grow thanks to the sponsors 
 ## License
 
 Nest is [MIT licensed](LICENSE).
+
+### -Setup New Project
+
+```
+npm i -g @nestjs/cli
+nest new project-name
+```
+
+```
+nest g resource customer
+```
+
+```
+npm i mongoose @nestjs/mongoose
+```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CustomerModule } from './customer/customer.module';
+
 
 @Module({
-  imports: [CustomerModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
