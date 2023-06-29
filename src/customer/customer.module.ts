004dd9664defd5651aec117e2b25a5267ca0aba5
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-29 21:54:17 +0530

    Update Content

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CustomerService } from './customer.service';
 import { CustomerController } from './customer.controller';
+import { CustomerSchema } from './schemas/customer.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Customer', schema: CustomerSchema }]),
+  ],
   controllers: [CustomerController],
-  providers: [CustomerService]
+  providers: [CustomerService],
 })
 export class CustomerModule {}
