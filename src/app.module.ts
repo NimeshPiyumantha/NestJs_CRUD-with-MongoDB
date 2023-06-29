06c1cdfa5ee7b7e892846875a08bdd46d71e41db
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-29 20:37:36 +0530

    Add customer.interface

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { CustomerModule } from './customer/customer.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { config } from 'dotenv';
+import { CustomerController } from './customer/customer.controller';
+import { CustomerService } from './customer/customer.service';
 // dotenv configuration
 config();
 
 @Module({
   imports: [CustomerModule, MongooseModule.forRoot(process.env.MongoDB_URI)],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CustomerController],
+  providers: [AppService, CustomerService],
 })
 export class AppModule {}
--- src/customer/customer.models.ts
+++ src/customer/customer.models.ts
@@ -1,3 +1,13 @@
-export class Customer{
-  user
-}
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'dotenv';
+import { CustomerController } from './customer.controller';
+import { CustomerService } from './customer.service';
+// dotenv configuration
+config();
+@Module({
+  imports: [MongooseModule.forRoot(process.env.MongoDB_URI)],
+  controllers: [CustomerController],
+  providers: [CustomerService],
+})
+export class CustomerModule {}
