ae61eb7e884e9c7d28c9ad025f4c03b2b5e53526
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-29 17:42:53 +0530

    Delete customer and Add terminal nest g resource customer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [],
+  imports: [CustomerModule],
   controllers: [AppController],
   providers: [AppService],
 })
